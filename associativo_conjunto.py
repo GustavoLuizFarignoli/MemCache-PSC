e51227990904634fbb982772c2f3860ee8a1b75f
GustavoLuizFarignoli <gustavolf.educ@gmail.com>
2023-05-07 10:58:01 -0300

    Função LFU

--- associativo_conjunto.py
+++ associativo_conjunto.py
@@ -35,7 +35,7 @@ def mapeamento_associativoconjunto(tamanho_cache, blocos, palavras, pos_memoria)
     cache = inicializar_cache(tamanho_cache,blocos,palavras)
     print(f"Tamanho da cache {tamanho_cache}") 
     imprimir_cache(cache)
-    print("Escolha a técnina de substituição:" + "\n-Lifo\n-Fifo\n-LRU")
+    print("Escolha a técnina de substituição:" + "\n-Lifo\n-Fifo\n-LRU\n-LFU")
     tec = input("->")
     print(tec.lower())
     if(tec.lower() == "lifo"):
@@ -44,6 +44,8 @@ def mapeamento_associativoconjunto(tamanho_cache, blocos, palavras, pos_memoria)
         hit, miss = fifo(tamanho_cache, palavras, pos_memoria, cache)
     elif(tec.lower() == "lru"):
         hit, miss = lru(tamanho_cache, palavras, pos_memoria, cache)
+    elif(tec.lower() == "lfu"):
+        hit, miss = lfu(tamanho_cache, blocos, palavras, pos_memoria, cache)
     print(f"- Total de posições de memórias acessadas: {len(pos_memoria)}")
     print(f"- Total de Hits: {hit}")
     print(f"- Total de Misses: {miss}")
@@ -145,6 +147,43 @@ def lru(tamanho_cache, palavras, pos_memoria, cache, hit=0, miss=0):
         time.sleep(10) #Pausa de alguns segundos para que o usuário possa acompanhar o processo
     return hit, miss
 
+def lfu(tamanho_cache, blocos, palavras, pos_memoria, cache, hit=0, miss=0):
+    lfu = {}
+    washit = False
+    for i in range(tamanho_cache):
+        lfubloco = []
+        for j in range(blocos):
+            lfubloco.append(0)
+        lfu[i] = lfubloco;
+    for i in range (len(pos_memoria)):
+        print(f"Linha {i} | Posição da memória requisitada: {pos_memoria[i]}")
+        x = math.floor((pos_memoria[i] / palavras) % tamanho_cache) #descobrindo o número do conjunto
+        for j in range (len(cache[x])):
+            if (pos_memoria[i] in cache[x][j]):
+                print("Status: Hit")
+                hit += 1
+                washit = True
+                lfu[x][j] += 1;
+                break
+        if (washit == False):
+            print("Status: Miss")
+            miss += 1
+            lowestfrequency = 100000
+            leastused = 0
+            for l in range (len(lfu[x])): # percorrer o lfu e ver qual bloco foi menos usado
+                if (lfu[x][l] < lowestfrequency):
+                    lowestfrequency = lfu[x][l]
+                    leastused = l
+            lfu[x][leastused] = 1
+            if ((pos_memoria[i] % 2) ==  0):
+                cache[x][leastused]= [pos_memoria[i],pos_memoria[i]+1]
+            else:
+                cache[x][leastused] [pos_memoria[i]-1,pos_memoria[i]]           
+        washit = False
+        imprimir_cache(cache)
+        print(lfu)
+        time.sleep(10) #Pausa de alguns segundos para que o usuário possa acompanhar o processo
+    return hit, miss
 
 
 tam, bloco, palavras, pos = menu_assocon()
